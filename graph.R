aafcce3d1fe1ebfadb27d3b242e89385a8b729ac
Christopher Gauthier <54082623+chrisgaut@users.noreply.github.com>
2024-03-15 23:20:57 -0700

    Per 100 Player Graph Improvements
    Put column chart on gradient scale to give hot/cold look, additional theme changes as well.

--- graph.R
+++ graph.R
@@ -19,6 +19,7 @@ team_ratings_graph <- function(year) {
     scale_x_continuous(limits = c(median_ortg - 10, median_ortg + 10)) +
     scale_y_continuous(limits = c(median_drtg - 10, median_drtg + 10)) +
     scale_y_reverse() + 
+    labs(caption = "Data from Basketball Reference | @chrisgaut9") +
     theme(panel.grid = element_line(color = "grey", size = 0.75, linetype = 1),
           panel.background = element_rect(fill = "white"),
           panel.border = element_rect(colour = "grey", fill=NA, size=1),
@@ -36,7 +37,19 @@ player_percentile_graph <- function(player, year) {
   
   # Plot
   player_percentile_plot <- ggplot(player_data) +
-    geom_col(aes(percentile, Stat), width = 0.6)  
+    #geom_col(aes(percentile, factor(Stat)), width = 0.6) +
+    geom_col(aes(x = percentile, y = factor(Stat), fill = percentile, stat = "identity")) +
+    scale_y_discrete(limits = c("TO/100", "AST/100", "FTA/100", "3PA/100", "2PA/100", "PTS/100")) +
+    xlim(0, 100) +
+    labs(title = "Per 100 Possessions Percentiles",
+         caption = "Data from Basketball Reference | @chrisgaut9") +
+    scale_fill_gradient2(low = "blue", high = "red", mid = "gray", midpoint = 50) +
+    theme(panel.grid = element_line(color = "grey", size = 0.75, linetype = 1),
+          panel.background = element_rect(fill = "white"),
+          panel.border = element_rect(colour = "grey", fill=NA, size=1),
+          text=element_text(family="source-sans-pro"),
+          axis.title.x = element_blank(),
+          axis.title.y = element_blank())
   
   return(player_percentile_plot)
 }
