d09a3761b2262be85e79c6a2cfa34586a1e2335c
Christopher Gauthier <54082623+chrisgaut@users.noreply.github.com>
2024-09-18 21:13:28 -0700

    Update tabs

--- ui.R
+++ ui.R
@@ -12,14 +12,16 @@ source('information.R')
 shinyUI(
   navbarPage(title="Basketball Wisdom",
              theme = shinytheme("cosmo"),
+             tabPanel("Visualization Lab", "Visualization Page"),
              tabPanel("League", 
                       plotOutput("teamRatingsPlot", width = "550px", height = "550px"),
                       gt_output("teamRatingsTable")),
              tabPanel("Player", 
                       selectInput("player", label = "Player", choices = player_list),
                       plotOutput("playerPercentilePlot", width = "550px", height = "550px")),
-             tabPanel("Team", "Team page"),
+             tabPanel("On Paper Matchup", "Team vs Team On Paper Visualization"),
              tabPanel("Model", "Model page"),
+             tabPanel("Betting Tools", "Page with different betting calculation tools"),
              tabPanel("About", "About page")
 )
 )
