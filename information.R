38a133215d6a430e4d15dd83af23dc0fb1b92cbe
Christopher Gauthier <54082623+chrisgaut@users.noreply.github.com>
2024-09-20 18:00:41 -0700

    Additional team viz vars and tab panel

--- information.R
+++ information.R
@@ -3,3 +3,10 @@ source('data.R')
 current_year <- 2024
 
 player_list <- get_per_100_poss(current_year)
+
+team_viz_lab_variable_list <- c('Age', 'Wins', 'Losses', 'Pythag. Wins', 'Pythag. Losses', 
+                                'Margin of Victory', 'Strength of Schedule', 'Simple Rating System',
+                                'Offensive Rating', 'Defensive Rating', 'Net Rating', 'Pace', 
+                                'FT Rate', '3PA Rate', 'True Shooting %', 'Effective FG %', 
+                                'Turnover %', 'Offensive Rebound %', 'FT/FGA', 'Opp. Effective FG %',
+                                'Opp. Turnover %', 'Defensive Rebound %', 'Opp. FT/FGA')
--- ui.R
+++ ui.R
@@ -13,15 +13,15 @@ shinyUI(
   navbarPage(title="Basketball Wisdom",
              theme = shinytheme("cosmo"),
              tabPanel("Visualization Lab", 
-                      selectInput("xAxisTeam", "X-Axis", choices = c("MOV", "SOS")),
-                      selectInput("yAxisTeam", "Y-Axis", choices = c("MOV", "SOS")),
-                      plotOutput("teamVizLabPlot")),
-             tabPanel("League", 
-                      plotOutput("teamRatingsPlot", width = "550px", height = "550px"),
-                      gt_output("teamRatingsTable")),
-             tabPanel("Player", 
+                      sidebarLayout(
+                        sidebarPanel(selectInput("xAxisTeam", "X-Axis", choices = team_viz_lab_variable_list, selected = "Offensive Rating"),
+                                     selectInput("yAxisTeam", "Y-Axis", choices = team_viz_lab_variable_list, selected = "Defensive Rating")),
+                        mainPanel(plotOutput("teamVizLabPlot", width = "550px", height = "550px"))
+                      )),
+             tabPanel("Player Profiles", 
                       selectInput("player", label = "Player", choices = player_list),
-                      plotOutput("playerPercentilePlot", width = "550px", height = "550px")),
+                      plotOutput("playerPercentilePlot", width = "550px", height = "550px")
+                      ),
              tabPanel("On Paper Matchup", "Team vs Team On Paper Visualization"),
              tabPanel("Model", "Model page"),
              tabPanel("Betting Tools", "Page with different betting calculation tools"),
